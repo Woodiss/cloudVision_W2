d911d508153e98d7a1d59458a8d5668f08354b09
Adrien Allard <allard15@hotmail.fr>
2025-05-08 15:49:22 +0200

    Add Management of the uploadForm component loading state

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-
+from flask_cors import CORS
 def create_app():
   app = Flask(__name__)
+  CORS(app, origins=["http://localhost:5173"], supports_credentials=True)
   # Importer les blueprints
   from .routes import main_routes
  
