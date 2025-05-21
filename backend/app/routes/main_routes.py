415f70a58af8dca177baa6515a0fe306d40b630a
Adrien Allard <allard15@hotmail.fr>
2025-05-21 15:16:48 +0200

    Add new component Result Recipe for the display of the result

--- backend/app/routes/main_routes.py
+++ backend/app/routes/main_routes.py
@@ -20,9 +20,12 @@ def home():
             # Récupérer les ingrédients ici
             ingredients = ["eggs", "strawberry", "flour", "sugar"]
 
+            #Récupérer l'image avec les bouding boxes
+            annotatedImage = "https://images.unsplash.com/photo-1745179276969-d9db2e682b5d?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
             try:
                 recipes = SpoonacularService.get_recipes_by_ingredients(ingredients)
-                return jsonify(recipes)
+                return jsonify(recipes, ingredients, annotatedImage)
             except Exception as e:
                 return jsonify({"error": str(e)}), 500
 
